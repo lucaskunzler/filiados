d3d1595196c2649df826cd6ca82f65b9e4d0e017
Lucas Kunzler <lucaskunzler@gmail.com>
2018-02-12 12:04:37 -0200

    cleaned up app.rb

--- app.rb
+++ app.rb
@@ -3,42 +3,36 @@ require 'zip'
 require 'open-uri'
 require 'json/ext'
 
-# ESTADOS = [
-#             'AC', 'AL', 'AP', 'AM', 'BA',
-#             'CE', 'DF', 'ES', 'GO', 'MA',
-#             'MT', 'MS', 'MG', 'PA', 'PB',
-#             'PR', 'PE', 'PI', 'RJ', 'RN',
-#             'RS', 'RO', 'RR', 'SC', 'SP',
-#             'SE', 'TO'
-#           ]
-# PARTIDOS = [
-#             'Avante', 'DEM', 'novo', 'pen', 'pc_do_b',
-#             'pcb', 'pco', 'pdt', 'phs', 'pmdb',
-#             'pmb', 'pmn', 'pp', 'ppl', 'pps',
-#             'pr', 'prb', 'pros', 'prp', 'prtb',
-#             'psb', 'psc', 'psd', 'psdb', 'psdc',
-#             'psl', 'psol', 'psol', 'pt', 'ptb',
-#             'ptc', 'pode', 'pv', 'rede', 'sd'
-#           ]
-ESTADOS = [ 'AC', 'AL' ]
-PARTIDOS = [ 'Avante', 'DEM', 'novo', 'pen', 'pc_do_b' ]
+STATES = [
+            'AC', 'AL', 'AP', 'AM', 'BA',
+            'CE', 'DF', 'ES', 'GO', 'MA',
+            'MT', 'MS', 'MG', 'PA', 'PB',
+            'PR', 'PE', 'PI', 'RJ', 'RN',
+            'RS', 'RO', 'RR', 'SC', 'SP',
+            'SE', 'TO'
+          ]
+PARTIES = [
+            'Avante', 'DEM', 'novo', 'pen', 'pc_do_b',
+            'pcb', 'pco', 'pdt', 'phs', 'pmdb',
+            'pmb', 'pmn', 'pp', 'ppl', 'pps',
+            'pr', 'prb', 'pros', 'prp', 'prtb',
+            'psb', 'psc', 'psd', 'psdb', 'psdc',
+            'psl', 'psol', 'psol', 'pt', 'ptb',
+            'ptc', 'pode', 'pv', 'rede', 'sd'
+          ]
+BASE_URI = "http://agencia.tse.jus.br/estatistica/sead/eleitorado/filiados/uf/"
 
-zip_urls = []
+def download(party, state)
+  tail = "filiados_#{party.downcase}_#{state.downcase}.zip"
+  IO.copy_stream(open(BASE_URI + tail), "#{tail}")
+end
 
-ESTADOS.each_with_index do |uf, index|
-  PARTIDOS.each_with_index do |partido, index|
-    puts "Baixando filiados do #{partido} no #{uf}"
-    url = "http://agencia.tse.jus.br/estatistica/sead/eleitorado/filiados/uf/filiados_#{partido.downcase}_#{uf.downcase}.zip"
-    download = open(url)
-    output_dir = "#{uf}/#{partido}"
-    Dir.mkdir(output_dir) unless Dir.exist?(output_dir)
-    Zip::File.open(download) do |zip_file|
-      # Handle entries one by one
-      zip_file.each do |entry|
-        # Extract to file/directory/symlink
-        puts "Extracting #{entry.name}"
-        entry.extract("#{File.expand_path(output_dir)}/#{entry.name}")
-      end
+def process(file)
+  Zip::File.open(file) do |zip_file|
+    zip_file.each do |entry|
+      next if entry.name == "aplic/sead/lista_filiados/uf/leiame.pdf"
+      content = entry.get_input_stream.read
+      puts content
     end
   end
 end
